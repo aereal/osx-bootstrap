4ca6ab66d9ec27f253cb322dd776067d3e491fb2
aereal <aereal@kerare.org>
2012-06-13 12:05:59 +0900

    Update directory

---
+++ bootstrap-perl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+__PERL_VERSION='perl-5.14.2'
+echo "Install perlbrew"
+curl -kL http://install.perlbrew.pl | bash
+
+echo "Install $__PERL_VERSION"
+perlbrew install $__PERL_VERSION
+
+echo "Install cpanm"
+perlbrew install-cpanm
+
+
+echo "Switch to $__PERL_VERSION"
+perlbrew switch $__PERL_VERSION
+
+cpanm --notest \
+  HTTP::Request::Common \
+  Growl::GNTP \
+  AnyEvent::HTTP \
+  Project::Libs \
+  Module::Install
