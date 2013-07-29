10a97376c751c3295683c47431b57e4588da7984
aereal <aereal@aereal.org>
2013-07-29 15:10:07 +0900

    Update osx.bash
    It supports only Mountain Lion

--- osx.bash
+++ osx.bash
@@ -1,19 +1,29 @@
-#!/bin/bash
+if sw_vers -productVersion | grep -vq '10[.]8'; then
+  echo "The script currently supports only OS X Mountain Lion (10.8)" > /dev/stderr
+  exit 1
+fi
 
 # フルキーボードアクセスを有効にする (Tabですべてのコントロールにフォーカスできるようになる)
 defaults write -g AppleKeyboardUIMode -int 3
 
+# Dock {{{
 # Dockを自動的に隠す
 defaults write com.apple.dock autohide -bool true
 
 # Dockをすぐに表示する
 defaults write com.apple.dock autohide-delay -float 0
 
-# Dockを2D表示に
-defaults write com.apple.dock no-glass -bool true
+# Minimize animation
+defaults write com.apple.dock mineffect scale
+
+# アプリケーションアイコンにしまう
+defaults write com.apple.dock minimize-to-application -bool true
 
 # 最小化されたアプリケーションのアイコンは透過させる
 defaults write com.apple.dock showhidden -bool true
+
+# Dockを2D表示に
+defaults write com.apple.dock no-glass -bool true
 
 # Dockを拡大する
 defaults write com.apple.dock magnification -bool true
@@ -24,27 +34,67 @@ defaults write com.apple.dock mouse-over-hilite-stack -bool true
 # 実行中のアプリケーションにインジケータを表示しないように
 defaults write com.apple.dock show-process-indicator -bool false
 
-# アンチエイリアスを有効に
-defaults -currentHost write -g AppleFontSmoothing -int 3
+# Dashboard をスペースとして表示しない
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# テキパキ Expose
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# disable dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+# }}}
+
+# Time Machine {{{
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+hash tmutil &>/dev/null && sudo tmutil disablelocal # Time Machine のローカルスナップショットを切る
+# }}}
+
+# Appearance {{{
+defaults write -g AppleAquaColorVariant -int 6 # Graphite
+# defaults write -g AppleHighlightColor # Highlight color
 
 # メニューバーを透過させないように
 defaults write -g AppleEnableMenuBarTransparency -bool false
 
 # スクロールバーはデバイスに応じて自動的に
 defaults write -g AppleShowScrollBars -string 'Automatic'
+# }}}
 
-# Finder
+# Fn キーを本来の動作にする
+defaults write -g com.apple.keyboard.fnState -bool true
+
+# Finder {{{
 # 拡張子を常に表示する
 defaults write -g AppleShowAllExtensions -bool true
 
-# Finderで検索するときは現在のディレクトリ以下から
+# 検索するときは現在のディレクトリ以下から
 defaults write com.apple.finder FXDefaultSearchScope -string 'SCcf'
+
+# フォルダの中身を QuickLook
+defaults write com.apple.finder QLEnableXRayFolders -bool true
+
+# Disable animations
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# 不可視ファイルも表示する
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# 便利バーは全部表示する
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+
+# リスト表示
+defaults write com.apple.finder FXPreferredViewStyle -string "Nslv"
 
 # ファイルに保存ダイアログは常に展開
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 
 # インターネットからダウンロードしたアプリケーションに実行ダイアログを表示しない
 defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# 拡張子変更にうるさくいわれない
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
+# }}}
 
 # 自動スペル訂正いらない
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
@@ -57,18 +107,21 @@ defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 # .DS_Storeをつくるな
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# 拡張子変更にうるさくいわれない
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
-
+# Security {{{
+# Screen Saver {{{
 # パスワードをすぐに求める
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
+# }}}
+# }}}
 
+# Trackpad {{{
 # タップでクリック
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 
 # ドラッグロック
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad DragLock -bool true
+# }}}
 
 # ウィンドウ状態の保存をしない
 defaults write -g NSQuitAlwaysKeepsWindows -bool false
@@ -81,6 +134,17 @@ defaults write com.apple.universalaccess mouseDriverCursorSize -int 4
 defaults write com.apple.ActivityMonitor IconType -int 4
 # }}}
 
+# App Store {{{
+defaults write com.apple.appstore ShowDebugMenu -bool true
+# }}}
+
+# 縮小するときのアニメーション
+# http://www.defaults-write.com/increase-the-speed-of-os-x-dialogs-boxes/#.UfSW3GSsgy4
+defaults write -g NSWindowResizeTime -float 0.001
+
+# Save to disk (not to iCloud)
+defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
+
 # Alfred.app {{{
 # ステータスバーのアイコンを隠す
 defaults write com.alfredapp.Alfred appearance.hideStatusBarIcon -bool true
@@ -91,17 +155,11 @@ defaults write com.alfredapp.Alfred appearance.hidePreferencesCogIcon -bool true
 # 帽子アイコンを隠す
 defaults write com.alfredapp.Alfred appearance.hideHat -bool true
 
-# Light theme
-defaults write com.alfredapp.Alfred appearance.themeuid -string 'alfred.theme.light'
-
 # enable eject command
 defaults write com.alfredapp.Alfred system.eject -bool true
 # }}}
 
 # Divvy.app {{{
-# hide menu-bar icon
-defaults write com.mizage.divvy showMenuIcon -bool false
-
 # ショートカットキーを押すたび複数ディスプレイ間を移動する
 defaults write com.mizage.divvy useMonitorCycling -bool true
 
@@ -110,26 +168,9 @@ defaults write com.mizage.divvy defaultColumnCount -int 10
 defaults write com.mizage.divvy defaultRowCount -int 10
 # }}}
 
-# Reeder.app {{{
-# open links in background
-defaults write com.reederapp.mac OpenLinksInBackground -bool true
+# Disable standby mode
+sudo pmset -a standbydelay 86400 # 24 hours
 
-# Disable services
-defaults write com.reederapp.mac ServiceAlternateBrowserDisabled -bool true
-defaults write com.reederapp.mac ServiceBlogDisabled -bool true
-defaults write com.reederapp.mac ServiceDeliciousDisabled -bool true
-defaults write com.reederapp.mac ServiceEvernoteDisabled -bool true
-defaults write com.reederapp.mac ServiceGoogleMobilizerDisabled -bool true
-defaults write com.reederapp.mac ServiceInstapaperDisabled -bool true
-defaults write com.reederapp.mac ServiceInstapaperMobilizerDisabled -bool true
-defaults write com.reederapp.mac ServiceMailLinkDisabled -bool true
-defaults write com.reederapp.mac ServicePinboardDisabled -bool true
-defaults write com.reederapp.mac ServiceReadabilityDisabled -bool true
-defaults write com.reederapp.mac ServiceSafariReadingListDisabled -bool true
-defaults write com.reederapp.mac ServiceTwitterDisabled -bool true
-defaults write com.reederapp.mac ServiceZootoolDisabled -bool true
+for app in Alfred Divvy Finder Dock SystemUIServer; do killall $app >/dev/null 2>&1; done
 
-defaults write com.reederapp.mac ServiceReadItLaterShowInSiderbar -bool true
-# }}}
-
-for app in Finder Dock SystemUIServer; do killall $app >/dev/null 2>&1; done
+# vim:set foldmethod=marker:
